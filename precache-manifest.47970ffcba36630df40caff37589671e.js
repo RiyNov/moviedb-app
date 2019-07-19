40d2babf691c53d2834dff1c8d38ae6a68b9086d
Riyandi Novian <43406693+RiyNov@users.noreply.github.com>
2019-07-19 13:45:36 +0700

    Update precache-manifest.47970ffcba36630df40caff37589671e.js

--- precache-manifest.47970ffcba36630df40caff37589671e.js
+++ precache-manifest.47970ffcba36630df40caff37589671e.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f3982c0251acab46ab50cf12f0ae5a97",
-    "url": "/moviedb-app/./index.html"
+    "url": "/moviedb-app/index.html"
   },
   {
     "revision": "0ab1bf293109f79da0b1",
-    "url": "/moviedb-app/./static/css/main.bb39a4fe.chunk.css"
+    "url": "/moviedb-app/static/css/main.bb39a4fe.chunk.css"
   },
   {
     "revision": "c747cb9434167600e5ca",
-    "url": "/moviedb-app/./static/js/2.29e3718e.chunk.js"
+    "url": "/moviedb-app/static/js/2.29e3718e.chunk.js"
   },
   {
     "revision": "0ab1bf293109f79da0b1",
-    "url": "/moviedb-app/./static/js/main.8b5a94da.chunk.js"
+    "url": "/moviedb-app/static/js/main.8b5a94da.chunk.js"
   },
   {
     "revision": "d3452e49b78c5519b169",
-    "url": "/moviedb-app/./static/js/runtime~main.84bbd308.js"
+    "url": "/moviedb-app/static/js/runtime~main.84bbd308.js"
   }
 ]);
